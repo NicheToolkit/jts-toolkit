76475ff59801f63ac730c1dbf386135e131f9f18
Cyan <snow22314@outlook.com>
2023-04-14 17:49:26 +0800

    jts fix

--- jts-toolkit-core/src/main/java/io/github/nichetoolkit/jts/serialization/GeometryDeserializer.java
+++ jts-toolkit-core/src/main/java/io/github/nichetoolkit/jts/serialization/GeometryDeserializer.java
@@ -1,11 +1,14 @@
 package io.github.nichetoolkit.jts.serialization;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import io.github.nichetoolkit.jts.JtsParser;
+import io.github.nichetoolkit.jts.JtsUtils;
 import io.github.nichetoolkit.jts.parser.GeometryParser;
+import io.github.nichetoolkit.rest.util.GeneralUtils;
 import org.locationtech.jts.geom.Geometry;
 
 import java.io.IOException;
@@ -29,6 +32,16 @@ public class GeometryDeserializer extends JsonDeserializer<Geometry> {
     @Override
     public Geometry deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
         JsonNode root = jsonParser.getCodec().readTree(jsonParser);
-        return jtsParser.parse(root);
+        JsonToken currentToken = jsonParser.getCurrentToken();
+        if (JsonToken.VALUE_STRING == currentToken) {
+            String geoJson = root.asText();
+            if (GeneralUtils.isEmpty(geoJson)) {
+                geoJson = root.toString();
+            }
+            return JtsUtils.parseGeojson(geoJson);
+        } else {
+
+            return jtsParser.parse(root);
+        }
     }
 }
