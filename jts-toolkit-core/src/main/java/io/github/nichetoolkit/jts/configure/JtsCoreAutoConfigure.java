4460d39a224c3197cc96f4db148e0bd9fa7c8dc6
Cyan <snow22314@outlook.com>
2025-07-17 10:53:20 +0800

    i18n

--- jts-toolkit-core/src/main/java/io/github/nichetoolkit/jts/configure/JtsCoreAutoConfigure.java
+++ jts-toolkit-core/src/main/java/io/github/nichetoolkit/jts/configure/JtsCoreAutoConfigure.java
@@ -1,18 +1,22 @@
 package io.github.nichetoolkit.jts.configure;
 
+import io.github.nichetoolkit.rest.RestI18n;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
 
 /**
  * <code>JtsCoreAutoConfigure</code>
  * <p>The jts core auto configure class.</p>
- * @see  lombok.extern.slf4j.Slf4j
- * @see  org.springframework.context.annotation.Configuration
- * @see  org.springframework.context.annotation.ComponentScan
- * @see  org.springframework.boot.autoconfigure.ImportAutoConfiguration
  * @author Cyan (snow22314@outlook.com)
+ * @see lombok.extern.slf4j.Slf4j
+ * @see org.springframework.context.annotation.Configuration
+ * @see org.springframework.context.annotation.ComponentScan
+ * @see org.springframework.boot.autoconfigure.ImportAutoConfiguration
  * @since Jdk1.8
  */
 @Slf4j
@@ -20,12 +24,32 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = {"io.github.nichetoolkit.jts"})
 @ImportAutoConfiguration(value = JtsJacksonAutoConfigure.class)
 public class JtsCoreAutoConfigure {
+
+    /**
+     * <code>JTS_I18N</code>
+     * {@link java.lang.String} <p>The constant <code>JTS_I18N</code> field.</p>
+     * @see java.lang.String
+     */
+    private static final String JTS_I18N = "jts-i18n";
+
     /**
      * <code>JtsCoreAutoConfigure</code>
      * <p>Instantiates a new jts core auto configure.</p>
      */
     public JtsCoreAutoConfigure() {
         log.debug("The auto configuration for [jts-core] initiated");
+    }
+
+    /**
+     * <code>jtsI18nBasename</code>
+     * <p>The jts i 18 n basename method.</p>
+     * @return {@link io.github.nichetoolkit.rest.RestI18n} <p>The jts i 18 n basename return object is <code>RestI18n</code> type.</p>
+     * @see io.github.nichetoolkit.rest.RestI18n
+     * @see org.springframework.context.annotation.Bean
+     */
+    @Bean
+    public RestI18n jtsI18nBasename() {
+        return () -> Collections.singleton(JTS_I18N);
     }
 
 }
--- jts-toolkit-core/src/main/java/io/github/nichetoolkit/jts/error/JtsErrorStatus.java
+++ jts-toolkit-core/src/main/java/io/github/nichetoolkit/jts/error/JtsErrorStatus.java
@@ -18,92 +18,90 @@ public enum JtsErrorStatus implements RestStatus {
      * <code>JTS_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>JTS_ERROR</code> field.</p>
      */
-    JTS_ERROR(10700, "it has encountered a error"),
+    JTS_ERROR(10700, "It has encountered a jts related error"),
     /**
      * <code>JTS_PARSE_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>JTS_PARSE_ERROR</code> field.</p>
      */
-    JTS_PARSE_ERROR(10701, "it has encountered a data parsing error"),
+    JTS_PARSE_ERROR(10701, "It has encountered a jts data parsing error"),
     /**
      * <code>JTS_BOX_INVALID_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>JTS_BOX_INVALID_ERROR</code> field.</p>
      */
-    JTS_BOX_INVALID_ERROR(10702, "the rang of box is invalid"),
+    JTS_BOX_INVALID_ERROR(10702, "The rang of jts box is invalid"),
 
     /**
      * <code>SHAPE_PARAMS_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>SHAPE_PARAMS_ERROR</code> field.</p>
      */
-    SHAPE_PARAMS_ERROR(10711, "it has encountered a param error"),
+    SHAPE_PARAMS_ERROR(10711, "It has encountered a shape param error"),
     /**
      * <code>SHAPE_DATA_STORE_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>SHAPE_DATA_STORE_ERROR</code> field.</p>
      */
-    SHAPE_DATA_STORE_ERROR(10712, "it has encountered a data store error"),
+    SHAPE_DATA_STORE_ERROR(10712, "It has encountered a shape data store error"),
     /**
      * <code>SHAPE_FEATURES_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>SHAPE_FEATURES_ERROR</code> field.</p>
      */
-    SHAPE_FEATURES_ERROR(10713, "it has encountered a features error"),
+    SHAPE_FEATURES_ERROR(10713, "It has encountered a shape features error"),
     /**
      * <code>SHAPE_PARAMS_UNINITIALIZED_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>SHAPE_PARAMS_UNINITIALIZED_ERROR</code> field.</p>
      */
-    SHAPE_PARAMS_UNINITIALIZED_ERROR(10714, "it has encountered a params uninitialized error"),
+    SHAPE_PARAMS_UNINITIALIZED_ERROR(10714, "It has encountered a shape params uninitialized error"),
     /**
      * <code>SHAPE_DATA_STORE_UNINITIALIZED_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>SHAPE_DATA_STORE_UNINITIALIZED_ERROR</code> field.</p>
      */
-    SHAPE_DATA_STORE_UNINITIALIZED_ERROR(10715, "it has encountered a data store uninitialized error"),
+    SHAPE_DATA_STORE_UNINITIALIZED_ERROR(10715, "It has encountered a shape data store uninitialized error"),
     /**
      * <code>SHAPE_FEATURES_UNINITIALIZED_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>SHAPE_FEATURES_UNINITIALIZED_ERROR</code> field.</p>
      */
-    SHAPE_FEATURES_UNINITIALIZED_ERROR(10716, "it has encountered a features uninitialized error"),
+    SHAPE_FEATURES_UNINITIALIZED_ERROR(10716, "It has encountered a shape features uninitialized error"),
     /**
      * <code>SHAPE_READER_UNINITIALIZED_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>SHAPE_READER_UNINITIALIZED_ERROR</code> field.</p>
      */
-    SHAPE_READER_UNINITIALIZED_ERROR(10717, "it has encountered a reader uninitialized error"),
+    SHAPE_READER_UNINITIALIZED_ERROR(10717, "It has encountered a shape reader uninitialized error"),
     /**
      * <code>SHAPE_WRITER_UNINITIALIZED_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>SHAPE_WRITER_UNINITIALIZED_ERROR</code> field.</p>
      */
-    SHAPE_WRITER_UNINITIALIZED_ERROR(10718, "it has encountered a writer uninitialized error"),
+    SHAPE_WRITER_UNINITIALIZED_ERROR(10718, "It has encountered a shape writer uninitialized error"),
 
     /**
      * <code>SHAPE_READER_PARAMS_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>SHAPE_READER_PARAMS_ERROR</code> field.</p>
      */
-    SHAPE_READER_PARAMS_ERROR(10719, "it has encountered a params error of shape file reader"),
+    SHAPE_READER_PARAMS_ERROR(10719, "It has encountered a params error of shape file reader"),
     /**
      * <code>SHAPE_READER_DATA_STORE_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>SHAPE_READER_DATA_STORE_ERROR</code> field.</p>
      */
-    SHAPE_READER_DATA_STORE_ERROR(10720, "it has encountered a data store error of shape file reader"),
+    SHAPE_READER_DATA_STORE_ERROR(10720, "It has encountered a data store error of shape file reader"),
     /**
      * <code>SHAPE_READER_FEATURES_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>SHAPE_READER_FEATURES_ERROR</code> field.</p>
      */
-    SHAPE_READER_FEATURES_ERROR(10721, "it has encountered a features error of shape file reader"),
+    SHAPE_READER_FEATURES_ERROR(10721, "It has encountered a features error of shape file reader"),
 
     /**
      * <code>SHAPE_WRITER_PARAMS_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>SHAPE_WRITER_PARAMS_ERROR</code> field.</p>
      */
-    SHAPE_WRITER_PARAMS_ERROR(10722, "it has encountered a params error of shape file writer"),
+    SHAPE_WRITER_PARAMS_ERROR(10722, "It has encountered a params error of shape file writer"),
     /**
      * <code>SHAPE_WRITER_DATA_STORE_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>SHAPE_WRITER_DATA_STORE_ERROR</code> field.</p>
      */
-    SHAPE_WRITER_DATA_STORE_ERROR(10723, "it has encountered a data store error of shape file writer"),
+    SHAPE_WRITER_DATA_STORE_ERROR(10723, "It has encountered a data store error of shape file writer"),
     /**
      * <code>SHAPE_WRITER_FEATURES_ERROR</code>
      * {@link io.github.nichetoolkit.jts.error.JtsErrorStatus} <p>The <code>SHAPE_WRITER_FEATURES_ERROR</code> field.</p>
      */
-    SHAPE_WRITER_FEATURES_ERROR(10724, "it has encountered a features error of shape file writer"),
-
-
+    SHAPE_WRITER_FEATURES_ERROR(10724, "It has encountered a features error of shape file writer"),
     ;
 
     /**
